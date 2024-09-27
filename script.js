5415ac48d8959bcedeedfc4ab83965ac5bc0edd1
Camila <zanellacamila016@gmail.com>
2024-09-27 12:16:09 -0300

    aula07

---
+++ script.js
@@ -0,0 +1,17 @@
+//setInterval
+// function acao(){
+//     document.write('Executando...</br>')
+// }
+// setInterval(acao,2000);
+
+//função anônima
+// setInterval(()=>{
+//     document.write('Executando2...</br>')
+
+
+// },3000)
+
+//para parar a execução
+var timer= setInterval(()=>{
+    document.write('Executando3...</br>');
+},1500);
